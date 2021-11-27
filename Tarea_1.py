b108f88e34cfedd26091ef8d8f44573c9ff1bbb7
Carlos_Sosa <charly100170@hotmail.com>
2021-11-27 02:53:21 -0600

    Main agregado

--- Tarea_1.py
+++ Tarea_1.py
@@ -1,28 +1,33 @@
-class Robot:
+def main():
 
-    def __init__(self, valor_articulacion, articulacion_nombre, robot_name):
-        self.valor_articulacion = valor_articulacion
-        self.articulacion_nombre = articulacion_nombre
-        self.robot_name = robot_name
+    class Robot:
 
-    def set_name(self):
-        self.robot_name = input("Introduce el nuevo nombre del robot:\n")
-        return self.robot_name
+        def __init__(self, valor_articulacion, articulacion_nombre, robot_name):
+            self.valor_articulacion = valor_articulacion
+            self.articulacion_nombre = articulacion_nombre
+            self.robot_name = robot_name
 
-    def get_articulacion(self):
-        return self.valor_articulacion
+        def set_name(self):
+            self.robot_name = input("Introduce el nuevo nombre del robot:\n")
+            return self.robot_name
+
+        def get_articulacion(self):
+            return self.valor_articulacion
         
 
-Pose = Robot([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
-              [0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 
-              [0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 
-              [0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 
-              [0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 
-              [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]], ["Art1", "Art2", "Art3", "Art4", "Art5", "Art6"], "ROS Robot")
+    Pose = Robot([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+                [0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 
+                [0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 
+                [0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 
+                [0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 
+                [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]], ["Art1", "Art2", "Art3", "Art4", "Art5", "Art6"], "ROS Robot")
 
-Pose.set_name()
-Pose.get_articulacion()
-print("Nombre del robot: ", Pose.robot_name)
+    Pose.set_name()
+    Pose.get_articulacion()
+    print("Nombre del robot: ", Pose.robot_name)
 
-for i in range (5):
-    print(Pose.articulacion_nombre[i], Pose.valor_articulacion[i])
+    for i in range (5):
+        print(Pose.articulacion_nombre[i], Pose.valor_articulacion[i])
+
+if __name__ == '__main__':
+    main()
